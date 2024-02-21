8078b966f9c379b8d6404f4b17f7b72263987fbe
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-02-20 22:24:35 -0500

    added boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { socket } from "./utils/socketClient";
+import {
+  createSuccessListeners,
+  onEndGame,
+  onJoinRoom,
+  onLeaveGame,
+  onNextTurn,
+  onStartGame,
+} from "./utils/functions";
 
 function App() {
-  const reducers = combineReducers();
+  // const reducers = combineReducers();
 
-  const store = configureStore(reducers);
+  // function that is run when the webpage loads
+  useEffect(() => {
+    socket.connect();
+    createSuccessListeners();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  // const store = configureStore(reducers);
 
   return (
     <ChakraProvider>
       <div>This is where we will create dungeons & dragons on the web</div>
+      <Button colorScheme="blue" onClick={onJoinRoom}>
+        Join Room
+      </Button>
+      <Button colorScheme="blue" onClick={onStartGame}>
+        Start Game
+      </Button>
+      <Button colorScheme="blue" onClick={onNextTurn}>
+        Next Turn
+      </Button>
+      <Button colorScheme="blue" onClick={onLeaveGame}>
+        Leave Game
+      </Button>
+      <Button colorScheme="blue" onClick={onEndGame}>
+        End Game
+      </Button>
     </ChakraProvider>
   );
 }
---
+++ src/utils/functions.js
@@ -0,0 +1,48 @@
+import { socket } from "./socketClient";
+
+// Let this page be the place you write all your functions, we might alter this file so that you can learn about redux
+// feel free to add arguments to functions or edit them in any way
+
+// the functions that have "on" are the ones that will emit messages
+// socket should emit join-room
+const onJoinRoom = () => {};
+
+// socket should emit join-room
+const onStartGame = () => {};
+
+// socket should emit join-room
+const onNextTurn = () => {};
+
+// socket should emit join-room
+const onLeaveGame = () => {};
+
+// socket should emit join-room
+const onEndGame = () => {};
+
+// this function is just to let the app know that these are the set of messages it should be listening for
+// i.e, these are all the socket.on functions
+const createSuccessListeners = () => {
+  // socket should listen for join-room-success (depends on what you wrote)
+  socket.on();
+
+  // socket should listen for start-game-success (depends on what you wrote)
+  socket.on();
+
+  // socket should listen for next-turn-success (depends on what you wrote)
+  socket.on();
+
+  // socket should listen for leave-game-success (depends on what you wrote)
+  socket.on();
+
+  // socket should listen for end-game-success (depends on what you wrote)
+  socket.on();
+};
+
+export {
+  onJoinRoom,
+  onStartGame,
+  onNextTurn,
+  onLeaveGame,
+  onEndGame,
+  createSuccessListeners,
+};
