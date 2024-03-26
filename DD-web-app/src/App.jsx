5b29cbc2e40881d8d1f7fa4697ca2588f6615467
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-03-26 16:26:00 -0400

    added changes

--- DD-web-app/src/App.jsx
+++ DD-web-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider, HStack, VStack } from "@chakra-ui/react";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { socket } from "./utils/socketClient";
 import {
@@ -15,11 +15,48 @@ import {
 
 function App() {
   // const reducers = combineReducers();
+  const [isPlayerTurn, setIsPlayerTurn] = useState();
+  const [prompt, setPrompt] = useState("");
+  const [options, setOptions] = useState(["", "", "", ""]);
+  const [roomId, setRoomId] = useState("12");
 
   // function that is run when the webpage loads
   useEffect(() => {
     socket.connect();
     createSuccessListeners();
+    socket.on(
+      "startGameSuccess",
+      ({ introduction, prompt, currentPlayerTurn, roundNumber, options }) => {
+        setPrompt(`${introduction} \n ${prompt}`);
+        setIsPlayerTurn(currentPlayerTurn === socket.id);
+        setOptions(options);
+        console.log(roundNumber);
+      }
+    );
+
+    socket.on("next-turn-success", ({ currentPlayerTurn, prompt }) => {
+      setIsPlayerTurn(currentPlayerTurn === socket.id);
+      setPrompt(prompt[0]);
+      setOptions([prompt[1], prompt[2], prompt[3], prompt[4]]);
+    });
+
+    window.addEventListener("unload", () => {
+      socket.emit("removePlayer", { roomId: roomId });
+    });
+
+    socket.on(
+      "end-round-success",
+      ({ currentPlayerTurn, introduction, prompt, options }) => {
+        setPrompt(` ${prompt}`);
+        setIsPlayerTurn(currentPlayerTurn === socket.id);
+        setOptions(options);
+      }
+    );
+
+    socket.on("end-game-success", ({ endGame, story }) => {
+      setPrompt(`You ${endGame}! \n ${story}`);
+      setIsPlayerTurn(false);
+    });
 
     return () => {
       socket.disconnect();
@@ -31,21 +68,51 @@ function App() {
   return (
     <ChakraProvider>
       <div>This is where we will create dungeons & dragons on the web</div>
-      <Button colorScheme="blue" onClick={onJoinRoom}>
+      <Button colorScheme="blue" onClick={() => onJoinRoom(roomId, "Laksh")}>
         Join Room
       </Button>
-      <Button colorScheme="blue" onClick={onStartGame}>
+      <Button
+        colorScheme="blue"
+        onClick={() =>
+          onStartGame(10, roomId, ["Fighter", "Mage", "Cleric", "Thief"])
+        }
+      >
         Start Game
       </Button>
-      <Button colorScheme="blue" onClick={onNextTurn}>
+      {/* <Button
+        colorScheme="blue"
+        onClick={() => onNextTurn("Used his sword to fight the dragon", roomId)}
+      >
         Next Turn
-      </Button>
-      <Button colorScheme="blue" onClick={onLeaveGame}>
+      </Button> */}
+      <Button colorScheme="blue" onClick={() => onLeaveGame(socket.id, roomId)}>
         Leave Game
       </Button>
-      <Button colorScheme="blue" onClick={onEndGame}>
+      <Button
+        colorScheme="blue"
+        onClick={() => onEndGame("All players died", roomId)}
+      >
         End Game
       </Button>
+      {prompt !== "" && (
+        <div>
+          <p>{prompt}</p>
+        </div>
+      )}
+
+      {isPlayerTurn && (
+        <VStack>
+          {options.map((option) => (
+            <Button
+              key={option}
+              colorScheme="blue"
+              onClick={() => onNextTurn(option, roomId)}
+            >
+              {option}
+            </Button>
+          ))}
+        </VStack>
+      )}
     </ChakraProvider>
   );
 }
--- DD-web-app/src/utils/functions.js
+++ DD-web-app/src/utils/functions.js
@@ -5,44 +5,73 @@ import { socket } from "./socketClient";
 
 // the functions that have "on" are the ones that will emit messages
 // socket should emit join-room
-const onJoinRoom = () => {
-  socket.emit("joinRoom", { roomId: "12", playerName: "Laksh" });
+const onJoinRoom = (roomId, playerName) => {
+  socket.emit("joinRoom", { roomId: roomId, playerName: playerName });
 };
 
 // socket should emit join-room
-const onStartGame = () => {};
+const onStartGame = (numRounds, roomId, roles) => {
+  socket.emit("startGame", {
+    numRounds: numRounds,
+    roomId: roomId,
+    roles: roles,
+  });
+};
 
 // socket should emit join-room
-const onNextTurn = () => {};
+const onNextTurn = (currentPlayerAction, roomId) => {
+  socket.emit("next-turn", {
+    currentPlayerAction: currentPlayerAction,
+    roomId: roomId,
+  });
+};
 
 // socket should emit join-room
-const onLeaveGame = () => {};
+const onLeaveGame = (playerId, roomId) => {
+  socket.emit("leaveGame", { playerThatLeft: playerId, roomId: roomId });
+};
 
 // socket should emit join-room
-const onEndGame = () => {};
+const onEndGame = (endGameReason, roomId) => {
+  socket.emit("end-game", { endGameReason: endGameReason, roomId: roomId });
+};
 
 // this function is just to let the app know that these are the set of messages it should be listening for
 // i.e, these are all the socket.on functions
 const createSuccessListeners = () => {
   // socket should listen for join-room-success (depends on what you wrote)
-  socket.on();
-
-  // socket should listen for start-game-success (depends on what you wrote)
-  socket.on();
-
-  // socket should listen for next-turn-success (depends on what you wrote)
-  socket.on();
-
-  // socket should listen for leave-game-success (depends on what you wrote)
-  socket.on();
-
-  // socket should listen for leave-game-success (depends on what you wrote)
-  socket.on("end-round", (newRoundNumber) => {
-    console.log(newRoundNumber);
+  socket.on("joinRoomSuccess", ({ players }) => {
+    console.log(players);
   });
 
-  // socket should listen for end-game-success (depends on what you wrote)
+  // socket should listen for start-game-success (depends on what you wrote)
+  // socket.on(
+  //   "startGameSuccess",
+  //   ({ prompt, currentPlayerTurn, roundNumber }) => {
+  //     console.log(prompt);
+  //     console.log(currentPlayerTurn);
+  //     console.log(roundNumber);
+  //   }
+  // );
+
+  // socket should listen for next-turn-success (depends on what you wrote)
+  // socket.on("next-turn-success", ({ currentPlayerTurn, prompt }) => {
+  //   console.log(currentPlayerTurn);
+  //   console.log(prompt);
+  // });
+
+  // socket should listen for leave-game-success (depends on what you wrote)
   socket.on();
+
+  // socket should listen for leave-game-success (depends on what you wrote)
+  // socket.on("end-round", ({ roundNumber }) => {
+  //   console.log(roundNumber);
+  // });
+
+  // socket should listen for end-game-success (depends on what you wrote)
+  // socket.on("end-game-success", ({ endGame, prompt }) => {
+  //   console.log(endGame, prompt);
+  // });
 };
 
 export {
