7ec501778d63ad4de19d73c5842883771966fe70
Alex <alexwang409@gmail.com>
2024-03-26 19:40:24 -0400

    updated landing page

---
+++ src/screens/LandingPage.jsx
@@ -0,0 +1,15 @@
+/* 
+functional component
+can also do:
+const MainPage = () => {
+    
+}
+
+TODO:
+- research React (watch a vid)
+- React Router
+- useState(), useEffect()
+- Redux (how to get data from the central state)
+*/
+
+function LandingPage() {}
