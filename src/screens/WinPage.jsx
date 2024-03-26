6563f662685e43959a7b1177b96b77a75ed03014
erika-tay <147895704+erika-tay@users.noreply.github.com>
2024-03-26 19:41:38 -0400

    Rules, Win, Lose Pages Started

---
+++ src/screens/WinPage.jsx
@@ -0,0 +1,12 @@
+function WinPage() {
+  // functional component
+  return (
+    <div>
+      <h1>You Won</h1>
+      <text>Hip Hip Hooray</text>
+
+      <button onClick={LandingPage}>Home</button>
+      <button onClick={MainPage}>Lobby</button>
+    </div>
+  );
+}
