249a1902a929cc8cfc0c6e9f63b4f380e00dabc9
ashu-jay <ashujay2004@gmail.com>
2024-04-09 18:56:46 -0400

    Fixed background

--- src/screens/MainPage.jsx
+++ src/screens/MainPage.jsx
@@ -1,89 +1,105 @@
-import React from 'react';
+import React from "react";
+import { Box, HStack, VStack, StackDivider } from "@chakra-ui/react";
 
+import backgroundImage from "../MainPageComponents/images/nature-paper-texture.png";
 import styles from "../MainPageComponents/PlayACTIVE.module.css";
 
-const MainPage = () => {
+const MainPage = ({ options, name }) => {
   return (
-    <div>
-      <div className="top-bar">Your Turn</div>
-      <div className={styles.playActive}>
-        <div className={styles.playActiveChild} />
-        <div className={styles.loremIpsumDolorContainer}>
-          <p
-            className={styles.loremIpsumDolor}
-          >{`Lorem ipsum dolor sit amet consectetur. Pharetra vestibulum laoreet facilisis non fermentum semper imperdiet amet malesuada. Cursus potenti vulputate eget egestas rutrum vitae. `}</p>
-          <p className={styles.loremIpsumDolor}>&nbsp;</p>
-          <p className={styles.loremIpsumDolor}>
-            Maecenas ut phasellus pulvinar id dui non dapibus hac cras. Non
-            praesent turpis amet eget libero. Nunc pellentesque feugiat at nibh
-            nunc nisi. Placerat sem quisque sit senectus in scelerisque facilisi.
-            Quisque sit diam orci fames at fringilla platea. Feugiat sit purus
-            massa habitant suspendisse mauris.
-          </p>
-          <p className={styles.loremIpsumDolor}>&nbsp;</p>
-          <p className={styles.loremIpsumDolor}>What will you do?</p>
-        </div>
-        <div className={styles.settings} />
+    <Box
+      backgroundImage={`url(${backgroundImage})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      width="100vw"
+      height="100vh"
+      position="fixed"
+      top="0"
+      left="0"
+    >
+      <VStack spacing={20}>
         <div className={styles.top}>
-          <div className={styles.topChild} />
           <div className={styles.yourTurn}>
             <span className={styles.yourTurnTxtContainer}>
               <b>Your</b>
-              <span className={styles.turn}> Turn</span>
+              <span className={styles.span}>{` `}</span>
+              <span>Turn</span>
             </span>
           </div>
-          <div className={styles.div}>0:47</div>
-          <img className={styles.topItem} alt="" src="/vector-46.svg" />
         </div>
-        <div className={styles.playActiveItem} />
-        <div className={styles.statsTab}>
-          <div className={styles.statsTabChild} />
-          <div className={styles.stats}>Stats</div>
-        </div>
-        <b className={styles.shreya}>Shreya</b>
-        <b className={styles.cleric}>Cleric</b>
-        <div className={styles.detailsTab}>
-          <div className={styles.detailsTabChild} />
-          <div className={styles.details}>Details</div>
-        </div>
-        <div className={styles.detailsTab1}>
-          <div className={styles.detailsTabItem} />
-          <div className={styles.roomInfo}>Room Info</div>
-        </div>
-        <div className={styles.healthStat}>
-          <div className={styles.health}>Health</div>
-          <div className={styles.div1}>75/100</div>
-          <div className={styles.healthBar}>
-            <div className={styles.healthBarChild} />
-            <div className={styles.healthBarItem} />
+
+        <HStack>
+          <div className={styles.loremIpsumDolorContainer}>
+            <p
+              className={styles.blankLine}
+            >{`Lorem ipsum dolor sit amet consectetur. Pharetra vestibulum laoreet facilisis non fermentum semper imperdiet amet malesuada. Cursus potenti vulputate eget egestas rutrum vitae. `}</p>
+            <p className={styles.blankLine}>&nbsp;</p>
+            <p className={styles.blankLine}>
+              Maecenas ut phasellus pulvinar id dui non dapibus hac cras. Non
+              praesent turpis amet eget libero. Nunc pellentesque feugiat at
+              nibh nunc nisi. Placerat sem quisque sit senectus in scelerisque
+              facilisi. Quisque sit diam orci fames at fringilla platea. Feugiat
+              sit purus massa habitant suspendisse mauris.
+            </p>
+            <p className={styles.blankLine}>&nbsp;</p>
+            <p className={styles.blankLine}>What will you do?</p>
           </div>
-        </div>
-        <div className={styles.charStat}>
-          <div className={styles.health}>Charisma</div>
-          <div className={styles.div1}>95/100</div>
-          <div className={styles.healthBar}>
-            <div className={styles.healthBarChild} />
-            <div className={styles.rectangleDiv} />
+          <div className={styles.choices}>
+            <div className={styles.choice1}>
+              <img
+                className={styles.naturePaperTexture1}
+                alt=""
+                src="/src/MainPageComponents/images/nature-paper-texture.png"
+              />
+              <b className={styles.loremIpsumDolor}>
+                Lorem ipsum dolor sit amet consectetur. Vulputate urna fusce
+                pellentesque aliquet et arcu pretium dignissim consectetur.
+                Tristique nunc suscipit donec tempus quam. Amet felis sit elit
+                nibh morbi quis et.
+              </b>
+            </div>
+            <div className={styles.choice2}>
+              <img
+                className={styles.naturePaperTexture1}
+                alt=""
+                src="/src/MainPageComponents/images/nature-paper-texture.png"
+              />
+              <b className={styles.loremIpsumDolor}>
+                Lorem ipsum dolor sit amet consectetur. Vulputate urna fusce
+                pellentesque aliquet et arcu pretium dignissim consectetur.
+                Tristique nunc suscipit donec tempus quam. Amet felis sit elit
+                nibh morbi quis et.
+              </b>
+            </div>
+            <div className={styles.choice3}>
+              <img
+                className={styles.naturePaperTexture1}
+                alt=""
+                src="/src/MainPageComponents/images/nature-paper-texture.png"
+              />
+              <b className={styles.loremIpsumDolor}>
+                Lorem ipsum dolor sit amet consectetur. Vulputate urna fusce
+                pellentesque aliquet et arcu pretium dignissim consectetur.
+                Tristique nunc suscipit donec tempus quam. Amet felis sit elit
+                nibh morbi quis et.
+              </b>
+            </div>
+            <div className={styles.choice4}>
+              <img
+                className={styles.naturePaperTexture1}
+                alt=""
+                src="/src/MainPageComponents/images/nature-paper-texture.png"
+              />
+              <b className={styles.loremIpsumDolor}>
+                Lorem ipsum dolor sit amet consectetur. Vulputate urna fusce
+                pellentesque aliquet et arcu pretium dignissim consectetur.
+                Tristique nunc suscipit donec tempus quam. Amet felis sit elit
+                nibh morbi quis et.
+              </b>
+            </div>
           </div>
-        </div>
-        <div className={styles.intelStat}>
-          <div className={styles.health}>Intelligence</div>
-          <div className={styles.div3}>60/100</div>
-          <div className={styles.healthBar}>
-            <div className={styles.healthBarChild} />
-            <div className={styles.healthBarChild2} />
-          </div>
-        </div>
-        <div className={styles.playActiveInner} />
-        <div className={styles.playActiveChild1} />
-        <div className={styles.playActiveChild2} />
-        <div className={styles.playActiveChild3} />
-        <div className={styles.scroll}>
-          <div className={styles.scrollChild} />
-          <div className={styles.scrollItem} />
-        </div>
-      </div>
-    </div>
+        </HStack>
+      </VStack>
+    </Box>
   );
 };
 
